ef3a32b5ddbf44cba4c18ac9f36b545fb6cb3959
VARDHAN_VALLURI <krishnavardhan124@gmail.com>
2026-01-12 04:29:48 +0530

    Update app_header.php

--- student/includes/app_header.php
+++ student/includes/app_header.php
@@ -1,11 +1,15 @@
 <div class="app-header">
-    <div class="back-container" onclick="goDashboard()">
-        <span class="back-icon">&#x2190;</span>
-    </div>
+
+    <?php if (!empty($showBackButton) && $showBackButton === true): ?>
+        <div class="back-container" onclick="goDashboard()">
+            <span class="back-icon">&#x2190;</span>
+        </div>
+    <?php endif; ?>
 
     <div class="app-title">
         <?php echo $pageTitle ?? 'Dashboard'; ?>
     </div>
+
 </div>
 
 <script>
